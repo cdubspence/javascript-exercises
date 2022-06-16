69a1d7861eb8fd03ad3b81f251201b2578dac3b2
Casey <orangobang@gmail.com>
2022-06-16 13:10:43 -0400

    first two done and passing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let newString =''
+    if (num < 0) {
+        return 'ERROR'
+    } else {
+        for (i = 0; i < num; i++) {
+            newString += word;
+        }
+    }
+    return newString
 };
 
 // Do not edit below this line
