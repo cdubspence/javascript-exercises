647a8047e27c08ab98080053b07d806864931d33
Casey Spence <orangobang@gmail.com>
2022-06-17 11:24:03 -0400

    sumAll completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let smallNum;
+    let bigNum;
+    if (Number.isInteger(num1) && Number.isInteger(num2)) {
+        if (num1 < 0 || num2 < 0) {
+            return 'ERROR'
+        }
+        else {
+            if (num1 < num2) {
+                smallNum = num1;
+                bigNum = num2
+            } else {
+                smallNum = num2;
+                bigNum = num1;
+            }
+        }
+    } 
+    else {
+        return 'ERROR'
+    }
 
+    for (i = smallNum; i <= bigNum; i++) {
+        sum += i
+    }
+    return sum;
 };
 
 // Do not edit below this line
